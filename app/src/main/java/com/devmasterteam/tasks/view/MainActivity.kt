a543cd51296cb36b638288ae7e091462d8c15408
Thierry Boiago <58778448+thierryboiago@users.noreply.github.com>
2023-12-15 16:52:24 -0300

    logout

--- app/src/main/java/com/devmasterteam/tasks/view/MainActivity.kt
+++ app/src/main/java/com/devmasterteam/tasks/view/MainActivity.kt
@@ -2,6 +2,7 @@ package com.devmasterteam.tasks.view
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.Gravity
 import com.google.android.material.navigation.NavigationView
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -10,19 +11,26 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.ui.NavigationUI
 import com.devmasterteam.tasks.R
 import com.devmasterteam.tasks.databinding.ActivityMainBinding
+import com.devmasterteam.tasks.viewmodel.MainViewModel
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
+    private lateinit var viewmodel: MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        viewmodel = ViewModelProvider(this).get(MainViewModel::class.java)
 
         setSupportActionBar(binding.appBarMain.toolbar)
 
@@ -55,6 +63,18 @@ class MainActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+        navView.setNavigationItemSelectedListener {
+            if(it.itemId == R.id.nav_logout){
+                viewmodel.logout()
+                startActivity(Intent(applicationContext, LoginActivity::class.java))
+                finish()
+            }else{
+                NavigationUI.onNavDestinationSelected(it, navController)
+                drawerLayout.closeDrawer(GravityCompat.START)
+            }
+
+            true
+        }
     }
 
     private fun observe() {
